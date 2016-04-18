8b29f44e9edd5cd2000a733c95004b19e8323194
yannBlanchard <yann.77.blanchard@gmail.com>
2016-04-18 23:16:39 +0200

    Ajout de test et detection d'un probleme casM3,J3,Y2

--- src/MyDateTest.java
+++ src/MyDateTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 /**
  * Created by Yann on 18/04/2016.
  */
-public class MyDateTest {
+public class MyDateTest{
     /*Jeux de test pour la fonction lendemain apres s'implification "Don't care"
     Mois 4 variants :
     M1 = 1,3,5,7,8,10
@@ -38,10 +38,83 @@ public class MyDateTest {
     @org.junit.Test
     public void nextDay() throws Exception {
         MyDate expectedDate = null;
+        //Cas M1,J1-4
         date = new MyDate(2,1,2016);
         date.nextDay();
         expectedDate = new MyDate(3,1,2016);
-        assertTrue(expectedDate.equals(date));
+        assertTrue("Cas M1,J1-4",expectedDate._day == date._day &&
+                expectedDate._month == date._month &&
+                expectedDate._year == date._year );
+        //Cas M1,J5
+        date = new MyDate(31,1,2016);
+        date.nextDay();
+        expectedDate = new MyDate(1,2,2016);
+        assertTrue("Cas M1,J5",expectedDate._day == date._day &&
+                expectedDate._month == date._month &&
+                expectedDate._year == date._year );
+        //Cas M2,J1-3
+        date = new MyDate(28,4,2016);
+        date.nextDay();
+        expectedDate = new MyDate(29,4,2016);
+        assertTrue("Cas M2,J1-3",expectedDate._day == date._day &&
+                expectedDate._month == date._month &&
+                expectedDate._year == date._year );
+        //Cas M2,J4
+        date = new MyDate(30,4,2016);
+        date.nextDay();
+        expectedDate = new MyDate(1,5,2016);
+        assertTrue("Cas M2,J4",expectedDate._day == date._day &&
+                expectedDate._month == date._month &&
+                expectedDate._year == date._year );
+
+        //Cas M2,J5  Erreur
+
+        //Cas M3,J1
+        date = new MyDate(1,2,2016);
+        date.nextDay();
+        expectedDate = new MyDate(2,2,2016);
+        assertTrue("Cas M3,J1",expectedDate._day == date._day &&
+                expectedDate._month == date._month &&
+                expectedDate._year == date._year );
+        //Cas M3,J2,Y1
+        date = new MyDate(28,2,2016);
+        date.nextDay();
+        expectedDate = new MyDate(29,2,2016);
+        assertTrue("Cas M3,J2,Y1",expectedDate._day == date._day &&
+                expectedDate._month == date._month &&
+                expectedDate._year == date._year );
+        //Cas M3,J2,Y2
+        date = new MyDate(28,2,2016);
+        date.nextDay();
+        expectedDate = new MyDate(1,3,2016);
+        assertTrue("Cas M3,J2,Y2",expectedDate._day == date._day &&
+                expectedDate._month == date._month &&
+                expectedDate._year == date._year );
+        //Cas M3,J3,Y1 Erreur
+
+        //Cas M3,J3,Y2
+        date = new MyDate(29,2,2016);
+        date.nextDay();
+        expectedDate = new MyDate(1,3,2016);
+        assertTrue("Cas M3,J3,Y2",expectedDate._day == date._day &&
+                expectedDate._month == date._month &&
+                expectedDate._year == date._year );
+        //Cas M3,J4-5 Erreur
+
+        //Cas M4,J1-4
+        date = new MyDate(1,12,2016);
+        date.nextDay();
+        expectedDate = new MyDate(2,12,2016);
+        assertTrue("Cas M4,J1-4",expectedDate._day == date._day &&
+                expectedDate._month == date._month &&
+                expectedDate._year == date._year );
+        //Cas M4-J5
+        date = new MyDate(31,12,2016);
+        date.nextDay();
+        expectedDate = new MyDate(1,1,2015);
+        assertTrue("Cas M4-J5",expectedDate._day == date._day &&
+                expectedDate._month == date._month &&
+                expectedDate._year == date._year );
     }
 
 }
