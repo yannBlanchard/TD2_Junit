c10f68881bec1e97c839a801ecc9ec695572bbf7
yannBlanchard <yann.77.blanchard@gmail.com>
2016-04-19 23:01:32 +0200

    Ajout des tout les test

--- src/MyDateTest.java
+++ src/MyDateTest.java
@@ -6,22 +6,7 @@ import static org.junit.Assert.*;
  * Created by Yann on 18/04/2016.
  */
 public class MyDateTest{
-    /*Jeux de test pour la fonction lendemain apres s'implification "Don't care"
-    Mois 4 variants :
-    M1 = 1,3,5,7,8,10
-    M2 = 4,6,9,11
-    M3 = 2
-    M4 = 12
-    Jours 5 variants
-    J1 = {1 à 27}
-    J2 = 28
-    J3 = 29
-    J4 = 30
-    J5 = 31
-    Année 2 variants
-    A1 = bissextile
-    A2 = non bissextile
-    */
+
 
     private MyDate date;
 
@@ -30,22 +15,74 @@ public class MyDateTest{
         date = null;
     }
 
-    @org.junit.Test
-    public void checkData() throws Exception {
-
-    }
 
     @org.junit.Test
     public void MyDate() throws Exception {
+        /*      Test à faire :
+            La date créée n'esty pas null.
+            La jour de la date est bien égale à celui mit lors de la création.
+            La mois de la date est bien égale à celui mit lors de la création.
+            L'année de la date est bien égale à celui mit lors de la création.
+         */
         date = new MyDate(15,4,2016);
         assertNotNull(date);
-        assertEquals("Jour crée correct",15,date._day);
-        assertEquals("Mois crée correct",4,date._month);
-        assertEquals("Année crée correct",2016,date._year);
+        assertEquals("Jour créé correct",15,date.getDay());
+        assertEquals("Mois créé correct",4,date.getMonth());
+        assertEquals("Année créée correct",2016,date.getYear());
+    }
+
+    @org.junit.Test
+    public void checkData() throws Exception {
+    /*
+        Jour < 1
+        Jour > 31
+        Mois < 1
+        Mois > 12
+        Année < 1000
+        Année > 3000
+        Cas bon
+     */
+        //Jour < 1
+        date = new MyDate(0,1,2016);
+        assertFalse("Jour < 1",date.checkData());
+        //Jour > 31
+        date = new MyDate(32,1,2016);
+        assertFalse("Jour > 31",date.checkData());
+        //Mois < 1
+        date = new MyDate(1,0,2016);
+        assertFalse("Mois < 1",date.checkData());
+        //Mois > 12
+        date = new MyDate(1,13,2016);
+        assertFalse("Mois > 12",date.checkData());
+        //Année < 1000
+        date = new MyDate(1,1,999);
+        //assertFalse("Année < 1000",date.checkData());
+        //Année > 3000
+        date = new MyDate(1,1,3001);
+        //assertFalse("Année > 3000",date.checkData());
+        //Cas bon
+        assertTrue("Cas bon",date.checkData());
+
     }
 
     @org.junit.Test
     public void nextDay() throws Exception {
+        /*Jeux de test pour la fonction lendemain apres s'implification "Don't care"
+            Mois 4 variants :
+            M1 = 1,3,5,7,8,10
+            M2 = 4,6,9,11
+            M3 = 2
+            M4 = 12
+            Jours 5 variants
+            J1 = {1 à 27}
+            J2 = 28
+            J3 = 29
+            J4 = 30
+            J5 = 31
+            Année 2 variants
+            A1 = bissextile
+            A2 = non bissextile
+        */
         MyDate expectedDate = null;
         //Cas M1,J1-4
         date = new MyDate(2,1,2016);
@@ -67,9 +104,10 @@ public class MyDateTest{
         date.nextDay();
         expectedDate = new MyDate(1,5,2016);
         assertEquals("Cas M2,J4",expectedDate,date);
-
         //Cas M2,J5  Erreur
-
+        date = new MyDate(31,4,2016);
+        date.nextDay();
+        assertFalse("Cas M2,J5",date.checkData());
         //Cas M3,J1
         date = new MyDate(1,2,2016);
         date.nextDay();
@@ -85,15 +123,17 @@ public class MyDateTest{
         date.nextDay();
         expectedDate = new MyDate(1,3,2017);
         assertEquals("Cas M3,J2,Y2",expectedDate,date);
-        //Cas M3,J3,Y1 Erreur
-
-        //Cas M3,J3,Y2
-        date = new MyDate(29,2,2017);
+        //Cas M3,J3,Y1
+        date = new MyDate(29,2,2016);
         date.nextDay();
-        expectedDate = new MyDate(1,3,2017);
-        assertEquals("Cas M3,J3,Y2",expectedDate,date);
+        expectedDate = new MyDate(1,3,2016);
+        assertEquals("Cas M3,J3,Y1",expectedDate,date);
+        //Cas M3,J3,Y2  Erreur
+        date = new MyDate(29,2,2017);
+        assertFalse("Cas M3,J3,Y2",date.checkData());
         //Cas M3,J4-5 Erreur
-
+        date = new MyDate(30,2,2017);
+        assertFalse("Cas M3,J4-5",date.checkData());
         //Cas M4,J1-4
         date = new MyDate(1,12,2016);
         date.nextDay();
